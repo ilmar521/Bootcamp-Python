f8d4f207ba435dca9a5ae3229bb3567fb87086a0
ilmar521 <ilmar521@mail.ru>
2023-01-15 21:29:38 +0200

    commit

---
+++ Week-3/Day-2/ExerciseXP/Ex4.js
@@ -0,0 +1,13 @@
+let c;
+let a = 34;
+let b = 21;
+
+console.log(a+b) //first expression
+// Prediction: 55
+// Actual: 55
+
+a = 2;
+
+console.log(a+b) //second expression
+// Prediction: 23
+// Actual: 23
