8e9a5dadddebd85cfa5d3ee033def8ed8f2fb214
ilmar521 <ilmar521@mail.ru>
2023-01-24 19:23:44 +0200

    commit

--- Week-4/Day-3/MiniProject/script.js
+++ Week-4/Day-3/MiniProject/script.js
@@ -3,7 +3,7 @@ let attempt = 0;
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
 }
 
 function compareNumbers(userNumber,computerNumber) {
