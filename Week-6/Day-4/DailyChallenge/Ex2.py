320715072963c3a6a7112f9dc8922f1acd5f32a7
ilmar521 <ilmar521@mail.ru>
2023-02-09 18:10:25 +0200

    Commit

--- Week-6/Day-4/DailyChallenge/Ex2.py
+++ Week-6/Day-4/DailyChallenge/Ex2.py
@@ -3,7 +3,7 @@ input_string = input('Enter a word: ')
 input_string_arr = list(input_string)
 input_string_arr2 = []
 for element in input_string_arr:
-    if element not in input_string_arr2:
+    if len(input_string_arr2) == 0 or input_string_arr2[-1] != element:
         input_string_arr2.append(element)
 
 print("".join(input_string_arr2))
