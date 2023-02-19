b2b929ed370938ed838d8d26fa3f2f1b98d8fafc
ilmar521 <ilmar521@mail.ru>
2023-02-19 19:53:16 +0200

    Commit

---
+++ Week-8/Day-4/ExcerciseXP/Ex1.py
@@ -0,0 +1,38 @@
+
+# Create a list called all_cats, which holds three cat instances : one Bengal, one Chartreux and one Siamese.
+# Those three cats are Sara’s pets. Create a variable called sara_pets which value is an instance of the Pet class, and pass the variable all_cats to the new instance.
+# Take all the cats for a walk, use the walk method.
+
+class Pets():
+    def __init__(self, animals):
+        self.animals = animals
+
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
+
+
+class Cat():
+    is_lazy = True
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def walk(self):
+        return f'{self.name} is just walking around'
+
+
+class Bengal(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+
+class Chartreux(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+
+class Siamese(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
