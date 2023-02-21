5d080425c1d3f36e3cabf9874c4008107e7aed20
ilmar521 <ilmar521@mail.ru>
2023-02-21 18:19:45 +0200

    Commit

--- Week-8/Day-2/DailyChallenge/script.py
+++ Week-8/Day-2/DailyChallenge/script.py
@@ -17,6 +17,23 @@ class Farm:
         final_str += '\n\tE-I-E-I-0!'
         return final_str
 
+    def get_animal_types(self):
+        list_types = []
+        for animal in self.animals.keys():
+            list_types.append(animal)
+        list_types.sort()
+        return list_types
+
+    def get_short_info(self):
+        list_types = self.get_animal_types()
+        final_str = 'McDonald’s farm has '
+        if len(list_types) == 1:
+            final_str += list_types[0]
+        else:
+            for x in range(len(list_types)):
+                final_str += f'and {list_types[x]}' if x == len(list_types) - 1 else f'{list_types[x]}, '
+        print(final_str)
+
 
 macdonald = Farm("McDonald")
 macdonald.add_animal('cow', 5)
@@ -24,3 +41,4 @@ macdonald.add_animal('sheep')
 macdonald.add_animal('sheep')
 macdonald.add_animal('goat', 12)
 print(macdonald.get_info())
+macdonald.get_short_info()
