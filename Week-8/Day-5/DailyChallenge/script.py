666b4d799e7c1300739b02b8a49455a8ef017436
ilmar521 <ilmar521@mail.ru>
2023-02-23 18:37:14 +0200

    Commit

---
+++ Week-8/Day-5/DailyChallenge/script.py
@@ -0,0 +1,31 @@
+
+import random
+
+
+class Card:
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+
+class Deck:
+
+    cards_in_deck = []
+
+    def shuffle(self):
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        values = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+        self.cards_in_deck.clear()
+        for x in suits:
+            for y in values:
+                self.cards_in_deck.append(Card(x, y))
+
+    def deal(self):
+        self.cards_in_deck.remove(random.choice(self.cards_in_deck))
+
+
+new_deck = Deck()
+new_deck.shuffle()
+print(len(new_deck.cards_in_deck))
+new_deck.deal()
+print(len(new_deck.cards_in_deck))
