aa82613788c070fcaa3e6d5834cd6468c486d59a
ilmar521 <ilmar521@mail.ru>
2023-02-23 19:16:10 +0200

    Commit

--- Week-8/Day-5/MiniProject/game.py
+++ Week-8/Day-5/MiniProject/game.py
@@ -9,8 +9,7 @@ class Game:
             value = input('Select (r)ock, (p)aper or (s)cissors: ')
             if value in list_of_values:
                 return value
-            else: # this else is redundant
-                print('You enter invalid value, please enter again')
+            print('You enter invalid value, please enter again')
 
     def get_computer_item(self):
         return random.choice(['r', 'p', 's'])
@@ -18,19 +17,18 @@ class Game:
     def get_game_result(self, user_item, computer_item):
         if user_item == computer_item:
             return 'draw'
-        else: # this else is redundant
-            if user_item == 'r' and computer_item == 'p':
-                return 'loss'
-            elif user_item == 'r' and computer_item == 's': # there is no need to type elif, just if because you do return
-                return 'win'
-            elif user_item == 'p' and computer_item == 's':
-                return 'loss'
-            elif user_item == 'p' and computer_item == 'r':
-                return 'win'
-            elif user_item == 's' and computer_item == 'r':
-                return 'loss'
-            elif user_item == 's' and computer_item == 'p':
-                return 'win'
+        if user_item == 'r' and computer_item == 'p':
+            return 'loss'
+        if user_item == 'r' and computer_item == 's':
+            return 'win'
+        if user_item == 'p' and computer_item == 's':
+            return 'loss'
+        if user_item == 'p' and computer_item == 'r':
+            return 'win'
+        if user_item == 's' and computer_item == 'r':
+            return 'loss'
+        if user_item == 's' and computer_item == 'p':
+            return 'win'
 
     def play(self):
         user_item = self.get_user_item()
