565907132014344acd8aad144065feb779cd611a
ayalCheckerInstitute <125178202+ayalCheckerInstitute@users.noreply.github.com>
2023-02-23 19:07:43 +0200

    checker-week8-day5-rock-paper-scissors
    Please see the comments that I added above

--- Week-8/Day-5/MiniProject/game.py
+++ Week-8/Day-5/MiniProject/game.py
@@ -9,7 +9,7 @@ class Game:
             value = input('Select (r)ock, (p)aper or (s)cissors: ')
             if value in list_of_values:
                 return value
-            else:
+            else: # this else is redundant
                 print('You enter invalid value, please enter again')
 
     def get_computer_item(self):
@@ -18,10 +18,10 @@ class Game:
     def get_game_result(self, user_item, computer_item):
         if user_item == computer_item:
             return 'draw'
-        else:
+        else: # this else is redundant
             if user_item == 'r' and computer_item == 'p':
                 return 'loss'
-            elif user_item == 'r' and computer_item == 's':
+            elif user_item == 'r' and computer_item == 's': # there is no need to type elif, just if because you do return
                 return 'win'
             elif user_item == 'p' and computer_item == 's':
                 return 'loss'
