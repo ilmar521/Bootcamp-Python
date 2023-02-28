31ae4ebb527f28ddd1ed5ddea712e18944695675
ilmar521 <ilmar521@mail.ru>
2023-02-28 20:36:05 +0200

    Commit

--- Week-9/Day-5/MiniProject/project.py
+++ Week-9/Day-5/MiniProject/project.py
@@ -1,5 +1,90 @@
 
 class Human:
+    id = 0
 
-    def __init__(self, ):
-        pass
+    def __init__(self, name, age, priority, blood_type):
+        self.id_number = Human.id
+        self.name = name
+        self.age = age
+        self.priority = priority
+        self.blood_type = blood_type
+        Human.id += 1
+
+class Queue:
+
+    def __init__(self):
+        self.humans = []
+
+    def add_person(self, person):
+        if person.age >= 60 or person.priority:
+            self.humans = [person] + self.humans
+        else:
+            self.humans.append(person)
+
+    def print(self):
+        list_print = []
+        for i in self.humans:
+            list_print.append(i.name)
+        print(" - ".join(list_print))
+
+    def find_in_queue(self, person):
+        for i, human in enumerate(self.humans):
+            if human == person:
+                return i
+        print('This person is not in queue!')
+        return None
+
+    def swap(self, person1, person2):
+        ind_1 = self.find_in_queue(person1)
+        ind_2 = self.find_in_queue(person2)
+        if ind_1 is not None and ind_2 is not None:
+            self.humans[ind_2] = person1
+            self.humans[ind_1] = person2
+
+    def get_next(self):
+        if len(self.humans) == 0:
+            return None
+        person = self.humans[0]
+        self.humans = [self.humans[i] for i in range(1, len(self.humans))]
+        return person
+
+    def get_next_blood_type(self, blood_type):
+        list_of_blood_type = list(filter(lambda human: human.blood_type == blood_type, self.humans))
+        if len(list_of_blood_type) == 0:
+            return None
+        person = list_of_blood_type[0]
+        self.humans = [human for human in self.humans if human != person]
+        return person
+
+    def sort_by_age(self):
+        list_of_priority = list(filter(lambda human: human.priority, self.humans))
+        list_of_regular = list(filter(lambda human: human.priority, self.humans))
+
+
+h1 = Human('H1', 18, False, 'A')
+h2 = Human('H2', 61, False, 'AB')
+h3 = Human('H3', 13, True, 'B')
+h4 = Human('H4', 34, False, 'AB')
+h5 = Human('H5', 56, False, 'B')
+h6 = Human('H6', 43, True, 'O')
+h7 = Human('H7', 65, False, 'A')
+
+queue = Queue()
+queue.add_person(h1)
+queue.add_person(h2)
+queue.add_person(h3)
+queue.add_person(h4)
+queue.add_person(h5)
+queue.add_person(h6)
+queue.add_person(h7)
+
+queue.print()
+queue.swap(h6, h1)
+queue.print()
+queue.get_next()
+queue.print()
+queue.add_person(h7)
+queue.get_next_blood_type('AB')
+queue.print()
+queue.add_person(h2)
+queue.print()
