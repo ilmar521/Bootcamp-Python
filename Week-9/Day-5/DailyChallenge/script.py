2724a933e8403e5e60671fbbdf65b3e02d4e3739
ilmar521 <ilmar521@mail.ru>
2023-03-02 12:12:49 +0200

    Commit

--- Week-9/Day-5/DailyChallenge/script.py
+++ Week-9/Day-5/DailyChallenge/script.py
@@ -1,3 +1,5 @@
+import datetime
+
 
 class Airline:
 
@@ -20,3 +22,56 @@ class Airplane:
         self.next_flights = []
         Airplane.ID += 1
 
+    def fly(self, destination):
+        pass
+
+    def location_on_date(self, date):
+        pass
+
+    def available_on_date(self, date, location):
+        pass
+
+
+class Flight:
+
+    def __init__(self, date:datetime.date, destination, origin, plane):
+        self.ID = destination.city + plane.company.id + str(date)
+        self.date = date
+        self.destination = destination
+        self.origin = origin
+        self.plane = plane
+
+    def take_off(self):
+        pass
+
+    def land(self):
+        pass
+
+class Airport:
+
+    def __init__(self, city):
+        self.city = city
+        self.planes = []
+        self.scheduled_departures = []
+        self.scheduled_arrivals = []
+
+    def schedule_flight(self, destination, date_time):
+        pass
+
+    def info(self, start_date, end_date):
+        pass
+
+
+avrora_air = Airline('AA', 'Avrora Airlines')
+elal_air = Airline('EA', 'ElAl Airlines')
+
+ben_gurion = Airport('Tel Aviv')
+shremetevo = Airport('Moscow')
+jfk = Airport('New York')
+
+aa_1 = Airplane(ben_gurion)
+aa_2 = Airplane(shremetevo)
+aa_3 = Airplane(shremetevo)
+ea_1 = Airplane(jfk)
+ea_2 = Airplane(ben_gurion)
+ea_3 = Airplane(shremetevo)
