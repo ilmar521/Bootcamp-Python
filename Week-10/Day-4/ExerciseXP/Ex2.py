a6b873457669aacb2aa446489a0cae0435dfb72e
ilmar521 <ilmar521@mail.ru>
2023-03-07 20:14:01 +0200

    Commit

--- Week-10/Day-4/ExerciseXP/Ex2.py
+++ Week-10/Day-4/ExerciseXP/Ex2.py
@@ -15,5 +15,5 @@ json_sampleJson = json.loads(sampleJson)
 print(json_sampleJson['company']['employee']['payable']['salary'])
 json_sampleJson['company']['employee']['birth_date'] = '1990-05-16'
 with open('json_file.json', 'w') as f:
-    json.dump(json_sampleJson, f)
+    json.dump(json_sampleJson, f, indent=4)
 
