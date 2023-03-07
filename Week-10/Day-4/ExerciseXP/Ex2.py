4b950ddb26ed8f0ec6448931c71add86e04c7f23
ilmar521 <ilmar521@mail.ru>
2023-03-07 19:16:56 +0200

    Commit

---
+++ Week-10/Day-4/ExerciseXP/Ex2.py
@@ -0,0 +1,15 @@
+import json
+sampleJson = """{ 
+   "company":{ 
+      "employee":{ 
+         "name":"emma",
+         "payable":{ 
+            "salary":7000,
+            "bonus":800
+         }
+      }
+   }
+}"""
+
+json_sampleJson = json.loads(sampleJson)
+print(json_sampleJson)
