c605a058c87fd78874a7bf396e487e95b4006d34
ilmar521 <ilmar521@mail.ru>
2023-03-08 20:51:22 +0200

    Commit

--- Week-10/Day-5/MiniProject/anagram_checker.py
+++ Week-10/Day-5/MiniProject/anagram_checker.py
@@ -10,7 +10,7 @@ class AnagramChecker:
         if response.status_code != 200:
             raise f'norvig.com return status code {response.status_code}!'
         list_of_words = response.text.split('\n')
-        AnagramChecker(list_of_words)
+        return AnagramChecker(list_of_words)
 
     def __init__(self, list_of_words):
         self.list_of_words = list_of_words
@@ -18,9 +18,9 @@ class AnagramChecker:
     def is_valid_word(self, word):
         return word.upper() in self.list_of_words
 
-    def get_anagrams(self, word):
-        all_annagrams = set(["".join(perm) for perm in itertools.permutations(word)])
-        list_of_annagrams = [word for word in all_annagrams if word.upper() in self.list_of_words]
+    def get_anagrams(self, analize_word):
+        all_annagrams = set(["".join(perm) for perm in itertools.permutations(analize_word)])
+        list_of_annagrams = [word for word in all_annagrams if word.upper() in self.list_of_words and word.upper() != analize_word.upper()]
         return list_of_annagrams
 
 
---
+++ Week-10/Day-5/MiniProject/anagrams.py
@@ -0,0 +1,26 @@
+from anagram_checker import AnagramChecker
+
+
+def main():
+    run_checker = True
+    checker = AnagramChecker.create_instance()
+    print("Hi! I'm anagram checker! You can enter any word that you want, and I'll word is it or not, and also show you all anargrams this word.")
+    while run_checker:
+        user_input = input('\nEnter the word or just enter "q" for finish: ').strip()
+        if user_input == 'q':
+            run_checker = False
+            print('Thanks for your time! Wait you soon.')
+            continue
+        if not user_input.isalpha():
+            print('You entered invalid word or several words!')
+            continue
+        if checker.is_valid_word(user_input):
+            print(f'\nYOUR WORD :”{user_input.upper()}”')
+            print('this is a valid English word.')
+            print(f'Anagrams for your word: {", ".join(checker.get_anagrams(user_input))}')
+        else:
+            print(f'\nYOUR WORD :”{user_input.upper()}”')
+            print('this is not a valid English word.')
+
+
+main()
