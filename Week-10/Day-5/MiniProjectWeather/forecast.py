0ad8bf0e8314be92a4c1d786aa104f8a41182ed3
ilmar521 <ilmar521@mail.ru>
2023-03-09 19:28:37 +0200

    Commit

--- Week-10/Day-5/MiniProjectWeather/forecast.py
+++ Week-10/Day-5/MiniProjectWeather/forecast.py
@@ -2,15 +2,32 @@ from pyowm.owm import OWM
 
 owm = OWM('f5d246278f5e139a1126a569d868bf58')
 mgr = owm.weather_manager()
+air_mgr = owm.airpollution_manager()
 
-# Search for current weather in London (Great Britain) and get details
-observation = mgr.weather_at_place('London,GB')
-w = observation.weather
 
-print(w.detailed_status)         # 'clouds'
-print(w.wind())                  # {'speed': 4.6, 'deg': 330}
-print(w.humidity)                 # 87
-print(w.temperature('celsius'))   # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
-print(w.rain)                    # {}
-print(w.heat_index)               # None
-print(w.clouds)                  # 75
+def init_plot():
+    pass
+
+
+def retrive_weather_and_forecast():
+
+    input_city = input('Enter a city that you wanna know weather and forecast: ')
+    try:
+        observation = mgr.weather_at_place(input_city)
+        w = observation.weather
+        hourly_forecast = mgr.forecast_at_place(input_city, '3h')
+    except:
+        raise "You entered invalid city"
+
+    print(f'Current weather situation in {input_city}')
+    print(f' - weather {w.detailed_status}')
+    print(f' - winds speed {w.wind()["speed"]}')
+    print(f" - sunrise time {w.sunrise_time(timeformat='date')}; sunset time {w.sunset_time(timeformat='date')}")
+    print('Forecast for next hours:')
+    for weather in hourly_forecast.forecast.weathers:
+        print(f" - weather for {weather.reference_time('iso'), weather.statusTT}")
+
+
+
+
+retrive_weather_and_forecast()
