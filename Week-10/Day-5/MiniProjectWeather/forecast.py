712d7e1806c57675d24c0e3dd903844f90a7f93b
ilmar521 <ilmar521@mail.ru>
2023-03-09 20:24:25 +0200

    Commit

--- Week-10/Day-5/MiniProjectWeather/forecast.py
+++ Week-10/Day-5/MiniProjectWeather/forecast.py
@@ -1,12 +1,28 @@
 from pyowm.owm import OWM
+from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
 
+plt.style.use('_mpl-gallery')
 owm = OWM('f5d246278f5e139a1126a569d868bf58')
 mgr = owm.weather_manager()
 air_mgr = owm.airpollution_manager()
 
 
 def init_plot():
-    pass
+    np.random.seed(3)
+    x = 0.5 + np.arange(8)
+    y = np.random.uniform(2, 7, len(x))
+
+    # plot
+    fig, ax = plt.subplots()
+
+    ax.bar(x, y, width=1, edgecolor="white", linewidth=0.7)
+
+    ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+           ylim=(0, 8), yticks=np.arange(1, 8))
+
+    plt.show()
 
 
 def retrive_weather_and_forecast():
@@ -30,4 +46,5 @@ def retrive_weather_and_forecast():
 
 
 
-retrive_weather_and_forecast()
+# retrive_weather_and_forecast()
+init_plot()
