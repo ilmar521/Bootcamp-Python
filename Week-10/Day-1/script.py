aaaacd4ac4ba18dd5d6cbaefc3c1c00afc2d7157
ilmar521 <ilmar521@mail.ru>
2023-03-10 12:29:17 +0200

    Commit

--- Week-10/Day-1/script.py
+++ Week-10/Day-1/script.py
@@ -1,13 +1,14 @@
 import os
 
-for i in range(1, 6):
-    os.mkdir(f'./Week-{i}')
-    os.chdir(f'./Week-{i}')
+for i in range(16, 17):
+    os.mkdir(f'../../Week-{i}')
+    os.chdir(f'../../Week-{i}')
     for n in range(1, 6):
         os.mkdir(f'./Day-{n}')
         os.chdir(f'./Day-{n}')
-        os.mkdir(f'./lesson')
-        os.mkdir(f'./homework')
+        if n == 2 or n == 4 or n ==5:
+            os.mkdir(f'./DailyChallenge')
+            os.mkdir(f'./ExerciseXP')
         os.chdir('../')
     os.chdir('../')
 
