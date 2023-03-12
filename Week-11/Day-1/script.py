aafb45c26e0f304003fbabce29461d5b8e38f215
ilmar521 <ilmar521@mail.ru>
2023-03-12 20:38:06 +0200

    Commit

--- Week-11/Day-1/script.py
+++ Week-11/Day-1/script.py
@@ -2,8 +2,21 @@ import flask
 
 app = flask.Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "HI!!"
+
+@app.route('/blog')
+def blog():
+    return "Welcome to my blog!"
+
+
+@app.route('/blog/articles')
+def blog_articles():
+    return '''
+        <ul>
+            <li>1 article</li>
+            <li>2 article</li>
+            <li>3 article</li>
+        <ul>
+    '''
+
 
 app.run()
