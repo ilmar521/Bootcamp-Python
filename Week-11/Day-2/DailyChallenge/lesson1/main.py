b8b6de0b39df0264c3f2e446552bcff2514a8252
ilmar521 <ilmar521@mail.ru>
2023-03-14 16:24:57 +0200

    Commit

---
+++ Week-11/Day-2/DailyChallenge/lesson1/main.py
@@ -0,0 +1,23 @@
+import flask
+import markdown as md
+
+app = flask.Flask(__name__)
+
+
+@app.route('/exercises')
+def blog():
+    with open("exercises.md") as md_obj:
+        md_str = md_obj.read()
+        html = md.markdown(md_str)
+        return html
+
+
+@app.route('/lesson')
+def blog_articles():
+    with open("in-this-chapter.md") as md_obj:
+        md_str = md_obj.read()
+        html = md.markdown(md_str)
+        return html
+
+
+app.run()
