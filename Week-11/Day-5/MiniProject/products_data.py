c76faa4e2358a1518a494b88140acdff1bcfae95
ayalCheckerInstitute <125178202+ayalCheckerInstitute@users.noreply.github.com>
2023-03-16 20:18:49 +0200

    week11-day5-ex
    Please see the above comments

--- Week-11/Day-5/MiniProject/products_data.py
+++ Week-11/Day-5/MiniProject/products_data.py
@@ -3,5 +3,5 @@ import json
 
 def load_database(src_file='product_db.json'):
     with open(src_file, 'r') as file_obj:
-        data = json.load(file_obj)
+        data = json.load(file_obj) # you can just do return at this line, like: return json.load(file_obj)
     return data
