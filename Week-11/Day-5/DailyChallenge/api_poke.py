f60ef9d571ea8b25a934929515edb580ccaa52c9
ilmar521 <ilmar521@mail.ru>
2023-03-20 16:37:50 +0200

    Commit

--- Week-11/Day-5/DailyChallenge/api_poke.py
+++ Week-11/Day-5/DailyChallenge/api_poke.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+
 def get_all_pokemon():
     data = []
     for i in range(10):
@@ -13,24 +14,21 @@ def get_all_pokemon():
 def get_pokemon(var):
     response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{var}')
     if response.status_code == 200:
-        return {'success':True, 'name': response.json()['species']['name'], 'img': response.json()['sprites']['front_default']}
-    return {'success':False}
+        return {'success': True, 'name': response.json()['species']['name'], 'img': response.json()['sprites']['front_default']}
+    return {'success': False}
 
 
 def get_types_pokemon():
     response = requests.get(f'https://pokeapi.co/api/v2/type/')
     if response.status_code == 200:
         types = response.json()
-        return {'success':True, 'types': [type_of_poke for type_of_poke in types["results"]]}
-    return {'success':False}
+        return {'success': True, 'types': [type_of_poke for type_of_poke in types["results"]]}
+    return {'success': False}
 
 
 def get_pokemon_by_type(url_type):
     response = requests.get(url_type)
     if response.status_code == 200:
         data = response.json()
-        return {'success':True, 'pokes': [poke['pokemon']['name'] for poke in data["pokemon"]]}
-    return {'success':False}
-
-#
-# get_pokemon_by_type('https://pokeapi.co/api/v2/type/2/')
+        return {'success': True, 'pokes': [poke['pokemon']['name'] for poke in data["pokemon"]]}
+    return {'success': False}
