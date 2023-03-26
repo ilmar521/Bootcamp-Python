4090a64180d71ba0e98ecadb55c178dd4a84ac20
ilmar521 <ilmar521@mail.ru>
2023-03-26 20:51:14 +0300

    Commit

--- Week-13/Day-2/MiniProject/app/__init__.py
+++ Week-13/Day-2/MiniProject/app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 import flask_sqlalchemy
 import flask_migrate
 import os
+import json
 
 flask_app = flask.Flask(__name__)
 flask_app.config.from_object(Config)
@@ -14,3 +15,17 @@ db = flask_sqlalchemy.SQLAlchemy(flask_app)
 migrate = flask_migrate.Migrate(flask_app, db)
 
 from app import models, routes
+
+
+# def populate():
+#     with flask_app.app_context():
+#         filename = os.path.join(flask_app.static_folder, 'users.json')
+#         with open(filename, 'r') as f:
+#             data = json.load(f)
+#             for user in data:
+#                 user_inst = models.User(id=user['id'], name=user['name'], street=user['address']['street'], zipcode=user['address']['zipcode'])
+#                 db.session.add(user_inst)
+#                 db.session.commit()
+#
+#
+# populate()
--- Week-13/Day-2/MiniProject/app/models.py
+++ Week-13/Day-2/MiniProject/app/models.py
@@ -2,7 +2,7 @@ from app import db
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150))
     street = db.Column(db.String(250))
     zipcode = db.Column(db.String(20))
