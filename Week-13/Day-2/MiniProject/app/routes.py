dcb3f6f06b41ca8f8bdaba1fe14f347306e01cdc
ilmar521 <ilmar521@mail.ru>
2023-03-28 13:26:14 +0300

    Commit

--- Week-13/Day-2/MiniProject/app/routes.py
+++ Week-13/Day-2/MiniProject/app/routes.py
@@ -1,6 +1,8 @@
 import flask
 from app import flask_app
 from app.models import User
+from app.forms import FormLogin
+from flask import flash
 
 
 @flask_app.route("/")
@@ -11,4 +13,26 @@ def index():
              'fourth': list(User.query.filter(User.zipcode.like('5%')))}
 
     return flask.render_template('index.html', users=users)
+
+
+@flask_app.route("/login", methods=("GET", "POST"))
+def login_page():
+    my_form = FormLogin()
+
+    if my_form.validate_on_submit():
+        name = my_form.name.data
+        if all(x.isalpha() or x.isspace() for x in name):
+            if User.query.filter(User.name == my_form.name.data).first() is not None:
+                flash('You logged in!', 'success')
+                return flask.redirect(flask.url_for('login_page'))
+            flash('You need to sign up!', 'error')
+            return flask.redirect(flask.url_for('add_user_page'))
+        else:
+            flash('Bad credentials!', 'error')
+    return flask.render_template("login.html", form=my_form)
+
+
+@flask_app.route("/add_user", methods=("GET", "POST"))
+def add_user_page():
+    pass
 
