e5f84c1ce375f48038e39152abcdff27a788cde9
ilmar521 <ilmar521@mail.ru>
2023-03-28 12:54:28 +0300

    Commit

--- Week-13/Day-2/MiniProject/app/routes.py
+++ Week-13/Day-2/MiniProject/app/routes.py
@@ -1,10 +1,14 @@
 import flask
 from app import flask_app
-from app import db
-from app.forms import FormCV
+from app.models import User
 
 
 @flask_app.route("/")
 def index():
-    return  flask.render_template_string('hello')
+    users = {'first': list(User.query.all()),
+             'second': list(User.query.filter(User.city == 'Roscoeview')),
+             'third': list(User.query.limit(5).all()),
+             'fourth': list(User.query.filter(User.zipcode.like('5%')))}
+
+    return flask.render_template('index.html', users=users)
 
