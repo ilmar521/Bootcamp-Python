fd7c8baaa013ee011664cdf5e017f5ff47e6cfa1
ilmar521 <ilmar521@mail.ru>
2023-03-29 15:26:40 +0300

    Commit

--- Week-13/Day-4/MiniProject/app/models.py
+++ Week-13/Day-4/MiniProject/app/models.py
@@ -1,16 +1,21 @@
 from app import db
+import datetime
 
 
-class Person(db.Model):
+class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(150))
-    email = db.Column(db.String(150))
-    phonenumbers = db.relationship('Phonenumber', backref='person', lazy='dynamic')
-    address = db.Column(db.String(150))
+    gender = db.Column(db.String(1))
+    breed = db.Column(db.String(150))
+    date_of_birth = db.Column(db.Text)
+    details = db.Column(db.DateTime, default=datetime.datetime.now())
+    price = db.Column(db.Integer)
+    image = db.Column(db.String(250))
+    cart = db.Column(db.Integer, db.ForeignKey('cart.id'))
 
 
-class Phonenumber(db.Model):
+class Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    number = db.Column(db.String(150))
-    owner = db.Column(db.Integer, db.ForeignKey('person.id'))
+    pets = db.relationship('Pet', backref='cart', lazy='dynamic')
+
 
--- Week-13/Day-4/MiniProject/app/routes.py
+++ Week-13/Day-4/MiniProject/app/routes.py
@@ -9,18 +9,6 @@ def index():
     return flask.render_template_string('Daily challenge')
 
 
-@flask_app.route("/person/<data>")
-def find_data(data):
-    phonenumber = Phonenumber.query.filter(Phonenumber.number == data).first()
-    if phonenumber is not None:
-        return flask.render_template('index.html', users=[phonenumber.person], phones=[phonenumber])
-    person = Person.query.filter(Person.name == data).first()
-    if person is not None:
-        return flask.render_template('index.html', users=[person], phones=list(person.phonenumbers.all()))
-
-    flash(f'Not find any data with - {data}')
-    return flask.render_template('index.html')
-
 
 
 
