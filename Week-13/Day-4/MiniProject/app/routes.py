f5be552cf50c61a004ca20fe80ab76ed6cc4570d
ilmar521 <ilmar521@mail.ru>
2023-03-29 20:32:26 +0300

    Commit

--- Week-13/Day-4/MiniProject/app/routes.py
+++ Week-13/Day-4/MiniProject/app/routes.py
@@ -5,10 +5,50 @@ from flask import flash
 
 
 @flask_app.route("/")
-def index():
-    return flask.render_template_string('Daily challenge')
+def homepage():
+    return flask.render_template('homepage.html')
 
 
+@flask_app.route("/pets")
+def pets_page():
+    return flask.render_template('pets.html', pets=data)
 
+
+@flask_app.route("/pet/<pet_id>")
+def pet_details_page(product_id):
+    list_product = list(filter(lambda product: product['ProductId'] == product_id, data))
+    return flask.render_template('product_details.html', product=list_product[0])
+
+
+@flask_app.route("/cart")
+def cart_page():
+    total = 0
+    for item in cart_items:
+        total += item['Price']
+    return flask.render_template('cart.html', cart_items=cart_items, total=total)
+
+
+# @app.route("/add_product_to_cart/<product_id>")
+# def add_product_page(product_id):
+#     list_product = list(filter(lambda product: product['ProductId'] == product_id, data))
+#     cart_items.append(list_product[0])
+#     return flask.render_template('products.html', products=data)
+#
+#
+# @app.route("/add_product_to_cart_from_products/<product_id>")
+# def add_product_from_products(product_id):
+#     list_product = list(filter(lambda product: product['ProductId'] == product_id, data))
+#     cart_items.append(list_product[0])
+#     return ("nothing")
+#
+#
+# @app.route("/delete_product_from_cart/<product_id>")
+# def delete_product_page(product_id):
+#     list_product = list(filter(lambda product: product['ProductId'] == product_id, data))
+#     cart_items.remove(list_product[0])
+#     total = 0
+#     for item in cart_items:
+#         total += item['Price']
+#     return flask.render_template('cart.html', cart_items=cart_items, total=total)
 
 
