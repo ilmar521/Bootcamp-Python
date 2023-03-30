e31d60d0dd35220df55da497985d4b44e03490d8
ilmar521 <ilmar521@mail.ru>
2023-03-30 13:36:41 +0300

    Commit

--- Week-13/Day-4/MiniProject/app/routes.py
+++ Week-13/Day-4/MiniProject/app/routes.py
@@ -20,41 +20,42 @@ def pets_page():
 
 @flask_app.route("/pet/<pet_id>")
 def pet_details_page(pet_id):
-    # list_product = list(filter(lambda product: product['ProductId'] == product_id, data))
-    # return flask.render_template('product_details.html', product=list_product[0])
+    pet = Pet.query.filter(Pet.id == pet_id).first()
+    return flask.render_template('pet.html', pet=pet)
     pass
 
 
 @flask_app.route("/cart")
 def cart_page():
-    pass
-#     total = 0
-#     for item in cart_items:
-#         total += item['Price']
-#     return flask.render_template('cart.html', cart_items=cart_items, total=total)
+    cart = Cart.query.filter(Pet.id == 1).first()
+    total = 0
+    pets = cart.pets.all()
+    for pet in pets:
+        total += pet.price
+    return flask.render_template('cart.html', cart_items=pets, total=total)
 
 
-# @app.route("/add_product_to_cart/<product_id>")
-# def add_product_page(product_id):
-#     list_product = list(filter(lambda product: product['ProductId'] == product_id, data))
-#     cart_items.append(list_product[0])
-#     return flask.render_template('products.html', products=data)
-#
-#
-# @app.route("/add_product_to_cart_from_products/<product_id>")
-# def add_product_from_products(product_id):
-#     list_product = list(filter(lambda product: product['ProductId'] == product_id, data))
-#     cart_items.append(list_product[0])
-#     return ("nothing")
-#
-#
-# @app.route("/delete_product_from_cart/<product_id>")
-# def delete_product_page(product_id):
-#     list_product = list(filter(lambda product: product['ProductId'] == product_id, data))
-#     cart_items.remove(list_product[0])
-#     total = 0
-#     for item in cart_items:
-#         total += item['Price']
-#     return flask.render_template('cart.html', cart_items=cart_items, total=total)
+@flask_app.route("/add_product_to_cart/<pet_id>")
+def add_pet_page(pet_id):
+    pet = Pet.query.filter(Pet.id == pet_id).first()
+    pet.cart_id = 1
+    db.session.add(pet)
+    db.session.commit()
+    flash(f'You have added {pet.name} to cart')
+    return flask.render_template('pets.html', pets=list(Pet.query.all()))
+
+
+@flask_app.route("/delete_pet_from_cart/<pet_id>")
+def delete_pet_page(pet_id):
+    pet = Pet.query.filter(Pet.id == pet_id).first()
+    pet.cart_id = 0
+    db.session.add(pet)
+    db.session.commit()
+    cart = Cart.query.filter(Pet.id == 1).first()
+    total = 0
+    pets = cart.pets.all()
+    for pet in pets:
+        total += pet.price
+    return flask.render_template('cart.html', cart_items=pets, total=total)
 
 
