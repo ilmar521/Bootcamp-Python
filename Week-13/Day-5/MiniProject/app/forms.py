9edd236d4fa97cee8dfa2adafb9220e3bb7b4828
ilmar521 <ilmar521@mail.ru>
2023-04-02 11:10:42 +0300

    Commit

--- Week-13/Day-5/MiniProject/app/forms.py
+++ Week-13/Day-5/MiniProject/app/forms.py
@@ -1,3 +1,7 @@
 import flask_wtf
 import wtforms
 
+
+class AddForm(flask_wtf.FlaskForm):
+    name = wtforms.StringField("Name", validators=[wtforms.validators.DataRequired()])
+    submit = wtforms.SubmitField("ADD")
--- Week-13/Day-5/MiniProject/app/models.py
+++ Week-13/Day-5/MiniProject/app/models.py
@@ -1,4 +1,4 @@
-from app import db
+from app import flask_app, db
 
 
 class Todo(db.Model):
@@ -6,4 +6,15 @@ class Todo(db.Model):
     details = db.Column(db.Text)
     completed = db.Column(db.Boolean)
 
+    def save_task_to_db(self):
+        with flask_app.app_context():
+            db.session.add(self)
+            db.session.commit()
+
+    def set_task_as_complete(self):
+        with flask_app.app_context():
+            self.completed = not self.completed
+            current_db_sessions = db.session.object_session(self)
+            current_db_sessions.add(self)
+            current_db_sessions.commit()
 
