b8159f0c98cb9aefc391a21b7fcc74eb797e7e34
ilmar521 <ilmar521@mail.ru>
2023-04-16 18:52:45 +0300

    Commit

--- Week-15/Day-1/Exercise/app/__init__.py
+++ Week-15/Day-1/Exercise/app/__init__.py
@@ -2,8 +2,9 @@ from flask import Flask
 
 flask_app = Flask(__name__)
 
-from app.filters import add_5_to_age
+from app.filters import add_5_to_age, add_domain
 
 flask_app.jinja_env.filters['add_5'] = add_5_to_age
+flask_app.jinja_env.filters['add_domain'] = add_domain
 
 from app import routes
