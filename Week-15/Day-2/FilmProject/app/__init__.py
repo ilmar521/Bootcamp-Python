7119643fef44bcfd4cd8f6d55fbfb5a7cf35b85c
ilmar521 <ilmar521@mail.ru>
2023-04-17 19:20:45 +0300

    Commit

--- Week-15/Day-2/FilmProject/app/__init__.py
+++ Week-15/Day-2/FilmProject/app/__init__.py
@@ -1,8 +1,21 @@
-from flask import Flask
+import flask
+from config import Config
+import flask_sqlalchemy
+import flask_migrate
+import os
 from app.films import films
 from app.accounts import accounts
 
-flask_app = Flask(__name__)
+flask_app = flask.Flask(__name__)
+flask_app.config.from_object(Config)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'IMDI.db')
+
+db = flask_sqlalchemy.SQLAlchemy(flask_app)
+migrate = flask_migrate.Migrate(flask_app, db)
 
 flask_app.register_blueprint(films, url_prefix="/films")
 flask_app.register_blueprint(accounts, url_prefix="/accounts")
+
+from app import models, routes
---
+++ Week-15/Day-2/FilmProject/app/models.py
@@ -0,0 +1,21 @@
+from app import db
+import datetime
+
+
+class Pet(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(150))
+    gender = db.Column(db.String(1))
+    breed = db.Column(db.String(150))
+    date_of_birth = db.Column(db.DateTime, default=datetime.datetime.now())
+    details = db.Column(db.Text)
+    price = db.Column(db.Integer)
+    image = db.Column(db.String(250))
+    cart_id = db.Column(db.Integer, db.ForeignKey('cart.id'))
+
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    pets = db.relationship('Pet', backref='cart', lazy='dynamic')
+
+
