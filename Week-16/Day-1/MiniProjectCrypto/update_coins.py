299f021d5c7b71a0010aecf3679e65c25c7a39bb
ilmar521 <ilmar521@mail.ru>
2023-04-27 17:38:44 +0300

    Commit

--- Week-16/Day-1/MiniProjectCrypto/update_coins.py
+++ Week-16/Day-1/MiniProjectCrypto/update_coins.py
@@ -1,12 +1,15 @@
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
+from app import create_app, db
+from app.models import Crypto
 
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
 parameters = {
   'start':'1',
-  'limit':'5000',
-  'convert':'USD'
+  'limit':'20',
+  'listing_status':'active',
+  'sort': 'cmc_rank'
 }
 headers = {
   'Accepts': 'application/json',
@@ -19,6 +22,15 @@ session.headers.update(headers)
 try:
   response = session.get(url, params=parameters)
   data = json.loads(response.text)
-  print(data)
+  list_coins = []
+  for coin in data['data']:
+    new_coin = Crypto(id=coin['id'], name=coin['name'], symbol=coin['symbol'], slug=coin['slug'], is_active=coin['is_active'], first_historical_data=coin['first_historical_data'], last_historical_data=coin['last_historical_data'])
+    list_coins.append(new_coin)
+
+  app = create_app()
+  with app.app_context():
+    db.session.add_all(list_coins)
+    db.session.commit()
+
 except (ConnectionError, Timeout, TooManyRedirects) as e:
   print(e)
